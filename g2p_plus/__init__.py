de4147a6653515e7e2e2cb910bd6d2ba898bafbb
Zeb <zepsilon32@gmail.com>
2025-04-02 16:43:03 +0100

    Add character_split_utterances

--- g2p_plus/__init__.py
+++ g2p_plus/__init__.py
@@ -4,7 +4,7 @@ g2p_plus.
 A G2P library for multiple language
 """
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = 'Zebulon Goriely'
 
-from .main import transcribe_utterances
+from .main import transcribe_utterances, character_split_utterances
